5cdfdbf1d6340cd1da9c75134123efa50232936c
dof-bull <donalofarrell1@gmail.com>
2020-08-23 18:59:29 +0000

    Added commissions views and templates.

--- showcase/views.py
+++ showcase/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Commission
 
 # Create your views here.
+
+
+def all_commissions(request):
+    """ A view to show all commissions, including sorting and search queries """
+
+    commissions = Commission.objects.all()
+
+    context = {
+        'commissions': commissions,
+    }
+
+    return render(request, 'commissions/commissions.html', context)
