f539b5bc29dca187db391f00f6364e66db3e46f0
dof-bull <donalofarrell1@gmail.com>
2020-08-23 18:27:11 +0000

    Customized admin.

--- showcase/admin.py
+++ showcase/admin.py
@@ -2,5 +2,26 @@ from django.contrib import admin
 from .models import Commission, Category
 
 # Register your models here.
-admin.site.register(Commission)
-admin.site.register(Category)
+
+
+class CommissionAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'sku',
+        'category',
+        'price',
+        'image',
+    )
+
+    ordering = ('name',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Commission, CommissionAdmin)
+admin.site.register(Category, CategoryAdmin)
