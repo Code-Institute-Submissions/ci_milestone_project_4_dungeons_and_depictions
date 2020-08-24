b8f31d7f4c27012b3b8352320ccb3e692f28e0c0
dof-bull <donalofarrell1@gmail.com>
2020-08-24 09:53:03 +0000

    Added search functionality.

--- showcase/views.py
+++ showcase/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Commission
 
 # Create your views here.
@@ -8,9 +10,21 @@ def all_commissions(request):
     """ A view to show all commissions, including sorting and search queries """
 
     commissions = Commission.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('commissions'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            commissions = commissions.filter(queries)
 
     context = {
         'commissions': commissions,
+        'search_term': query,
     }
 
     return render(request, 'commissions/commissions.html', context)
