933eef3f01023abcc84e7fe18fbf8efa04ef50b6
dof-bull <donalofarrell1@gmail.com>
2020-08-26 10:22:14 +0000

    Successfully cloned repo to new one do to typo in project name. Add secret key to hidden env.py file.

--- dungeons_and_depictions/settings.py
+++ dungeons_and_depictions/settings.py
@@ -12,16 +12,18 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+from os import path
+if path.exists("env.py"):
+  import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wx#(wf(%ii8x2d36=-kc*1eal-7)3z$7)s(-5tht2b-bdb49-3'
+SECRET_KEY = "os.environ.get('SECRET_KEY')"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
