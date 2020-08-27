17ae00da41e8aa2d825130543e9992503833519a
dof-bull <donalofarrell1@gmail.com>
2020-08-27 16:10:08 +0000

    Added about page view, url and template and linked it in nav.

--- home/views.py
+++ home/views.py
@@ -6,3 +6,8 @@ from django.shortcuts import render
 def index(request):
     """ This view returns the index page """
     return render(request, 'home/index.html')
+
+
+def about(request):
+    """ This view returns the about page """
+    return render(request, 'home/about.html')
