ec6386b5f1c250a43d79e91b04470636814bec40
dof-bull <donalofarrell1@gmail.com>
2020-08-28 20:06:35 +0000

    Contact page full functional and styled as crispy form.

--- home/forms.py
+++ home/forms.py
@@ -1,10 +1,21 @@
 from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 
 
 class ContactForm(forms.Form):
     contact_name = forms.CharField(required=True)
     contact_email = forms.EmailField(required=True)
-    content = forms.CharField(
-        required=True,
-        widget=forms.Textarea
-    )
+    contact_order_number = forms.CharField(max_length=254, required=False)
+    content = forms.CharField(required=True, widget=forms.Textarea)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_id = 'id-contact form'
+
+        self.fields['contact_name'].label = "Name:"
+        self.fields['contact_email'].label = "Email:"
+        self.fields['contact_order_number'].label = "Order Number (if applicable):"
+        self.fields['content'].label = "How can we help?"
+        self.helper.add_input(Submit('submit', 'Submit'))
--- home/views.py
+++ home/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.mail import EmailMessage
+from django.template.loader import get_template
+from django.contrib import messages
 from .forms import ContactForm
 
 
@@ -14,6 +17,41 @@ def about(request):
 
 def contact(request):
     form_class = ContactForm
+
+    if request.method == 'POST':
+        form = form_class(data=request.POST)
+
+        if form.is_valid():
+            messages.success(request, 'Your email has been sent!')
+            contact_name = request.POST.get(
+                'contact_name', '')
+            contact_email = request.POST.get(
+                'contact_email', '')
+            contact_order_number = request.POST.get(
+                'contact_order_number', '')
+            form_content = request.POST.get('content', '')
+
+            template = get_template('contact_template.txt')
+            context = {
+                'contact_name': contact_name,
+                'contact_email': contact_email,
+                'contact_order_number': contact_order_number,
+                'form_content': form_content,
+            }
+            content = template.render(context)
+
+            email = EmailMessage(
+                "New contact form submission",
+                content,
+                "Your website" + '',
+                ['youremail@gmail.com'],
+                headers={'Reply-To': contact_email}
+            )
+            email.send()
+            return redirect('contact')
+        else:
+            messages.error(
+                request, 'Failed to send email. Please ensure the form is valid.')
 
     return render(request, 'home/contact.html', {
         'form': form_class,
