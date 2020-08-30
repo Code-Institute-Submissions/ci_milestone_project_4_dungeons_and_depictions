d7915c3ac8fe1787321ec2c8dcfac4192882d151
dof-bull <donalofarrell1@gmail.com>
2020-08-30 07:50:02 +0000

    Added admin button to superuser view. Added default sku. Added home button to iPad viewing.

--- showcase/models.py
+++ showcase/models.py
@@ -18,7 +18,8 @@ class Category(models.Model):
 
 class Commission(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-    sku = models.CharField(max_length=254, null=True, blank=True)
+    sku = models.CharField(max_length=254, null=True,
+                           blank=True, default="PENDING")
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2, blank=True, default=50.00)
